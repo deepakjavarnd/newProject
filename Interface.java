d3165c91998f4bddcbffa198475a2a77560afab0
deepakjavarnd <35270861+deepakjavarnd@users.noreply.github.com>
2018-03-29 19:39:13 +0530

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -3,15 +3,7 @@ package javarnd.Cocept;
 public interface Interface {
 	public void m();
 	public void m2();
-<<<<<<< HEAD
-<<<<<<< HEAD
 	public void m3();
-=======
 	public void firstBranch1();
 	public void firstBranch2();
->>>>>>> FirstBranch modified
-=======
-	public void firstBranch1();
-	public void firstBranch2();
->>>>>>> 54c5616af1334a89ca8fb47a3ade2d429e64b733
 }
